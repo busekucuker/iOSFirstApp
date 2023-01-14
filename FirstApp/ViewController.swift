0f52fadbff43d60fc0a78338cdf3b6d30e0f33b7
busekucuker <busekucukerr@outlook.com>
2023-01-14 21:07:01 +0300

    FirstApp iOS

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var FoodDeliveryLabel: UILabel!
+    
+    @IBOutlet weak var TacoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func ClickButton(_ sender: Any) {
+        TacoImage.image = UIImage (named: "Pasta")
+    }
 }
 
